045e8abd20ca33a656fd383e09c4d733edca6b4b
abhat@J9978R1-ABHAT.corp.zynga.com <abhat@J9978R1-ABHAT.corp.zynga.com>
2012-04-28 23:18:03 +0530

    Removed all the header additions.

--- download.py
+++ download.py
@@ -20,18 +20,7 @@ class Config(object):
 
 cj = cookielib.CookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-opener.addheaders = [
-('Host', 'www.coursera.org'),
-('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'),
-('Accept-Language', 'en-us,en;q=0.5'),
-('Pragma', 'no-cache'),
-('Connection', 'keep-alive'),
-('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0')
-]
 
-#r = opener.open('https://www.coursera.org/')
-#r = opener.open('https://www.coursera.org/')
-#r = opener.open('https://www.coursera.org/account/signin')
 r = opener.open('https://www.coursera.org/account/signin')
 r = opener.open('https://www.coursera.org/maestro/auth/api/user/csrf_token')
 
@@ -55,21 +44,12 @@ if True:
     if True:
         formParams = urllib.urlencode(formParams)
 
-        opener.addheaders = [
-        ('Referer', 'https://www.coursera.org/account/signin'),
-        ('X-Requested-With',	'XMLHttpRequest')
-        ]
-
         r = opener.open('https://www.coursera.org/maestro/auth/api/user/login', formParams)
         #print r.read()
 
         #r = opener.open('https://www.coursera.org/maestro/auth/api/user/login', formParams)
         #print "------------2>", cj
         #r = opener.open('https://www.coursera.org/')
-
-        opener.addheaders = [
-        ('Host', 'class.coursera.org'),
-        ]
 
         print cj
 
