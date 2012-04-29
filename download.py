13cb990435854b7352542189113c9bb05a64fd1b
abhat@J9978R1-ABHAT.corp.zynga.com <abhat@J9978R1-ABHAT.corp.zynga.com>
2012-04-29 10:21:28 +0530

    Refactoring.

--- download.py
+++ download.py
@@ -1,4 +1,4 @@
-import cookielib, urllib2, urllib, ConfigParser
+import cookielib, urllib2, urllib, ConfigParser, pprint
 
 class Config(object): 
     SECTION_CREDENTIALS = 'credentials'
@@ -10,12 +10,22 @@ class Config(object):
 
     CONFIG_FILE = 'download.cfg'
 
+    LOGIN_URL = 'https://www.coursera.org/maestro/auth/api/user/login'
+    REDIRECT_URL = 'https://class.coursera.org/{0}/auth/auth_redirector?type=login&subtype=normal&email=&visiting=%2F{0}%2Flecture%2Findex&minimal=true'
+
     def __init__(self):
         config = ConfigParser.ConfigParser()
         config.read(Config.CONFIG_FILE)
         self.username = config.get(Config.SECTION_CREDENTIALS, Config.SECTION_CREDENTIALS_USERNAME)
         self.password = config.get(Config.SECTION_CREDENTIALS, Config.SECTION_CREDENTIALS_PASSWORD)
         self.course = config.get(Config.SECTION_DOWNLOAD, Config.SECTION_DOWNLOAD_CLASS)
+        self.clazz = self.getJustClass(self.course)
+        self.loginURL = Config.LOGIN_URL
+        self.redirectURL = Config.REDIRECT_URL.format(self.clazz)
+
+    def getJustClass(self, courseURL):
+        splits = courseURL.split('/')
+        return splits[3]
 
 def getVideoPage(config):
     cj = cookielib.CookieJar()
@@ -28,12 +38,14 @@ def getVideoPage(config):
 
     formParams = urllib.urlencode(formParams)
 
-    opener.open('https://www.coursera.org/maestro/auth/api/user/login', formParams)
-    opener.open('https://class.coursera.org/algo/auth/auth_redirector?type=login&subtype=normal&email=&visiting=%2Falgo%2Flecture%2Findex&minimal=true')
+    print config.loginURL
+    opener.open(config.loginURL, formParams)
+    print config.redirectURL
+    opener.open(config.redirectURL)
     r = opener.open(config.course)
 
     return r.read()
 
-
 config = Config()
 print getVideoPage(config)
+#getJustClass(config)
