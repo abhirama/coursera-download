13ae221e141dd8599113ba061a9ea46135090007
abhat@J9978R1-ABHAT.corp.zynga.com <abhat@J9978R1-ABHAT.corp.zynga.com>
2012-04-29 10:06:52 +0530

    Removed all unnecessary url calls.

--- download.py
+++ download.py
@@ -17,47 +17,21 @@ class Config(object):
         self.password = config.get(Config.SECTION_CREDENTIALS, Config.SECTION_CREDENTIALS_PASSWORD)
         self.course = config.get(Config.SECTION_DOWNLOAD, Config.SECTION_DOWNLOAD_CLASS)
 
-
 cj = cookielib.CookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 
-r = opener.open('https://www.coursera.org/account/signin')
-r = opener.open('https://www.coursera.org/maestro/auth/api/user/csrf_token')
+config = Config()
+formParams = {
+    'email_address': config.username,
+    'password': config.password,
+}
 
-#print "------------1>", cj
+formParams = urllib.urlencode(formParams)
 
-if True:
-    csrfTokenValue = ''
-    for ck in cj:
-        if ck.name == 'csrf_token':
-            csrfTokenValue = ck.value
+opener.open('https://www.coursera.org/maestro/auth/api/user/login', formParams)
+opener.open('https://class.coursera.org/algo/auth/auth_redirector?type=login&subtype=normal&email=&visiting=%2Falgo%2Flecture%2Findex&minimal=true')
+r = opener.open(config.course)
 
-    #print "Token value:", csrfTokenValue
-
-    config = Config()
-    formParams = {
-        'email_address': config.username,
-        'password': config.password,
-        'csrf_token': csrfTokenValue
-    }
-
-    if True:
-        formParams = urllib.urlencode(formParams)
-
-        r = opener.open('https://www.coursera.org/maestro/auth/api/user/login', formParams)
-        #print r.read()
-
-        #r = opener.open('https://www.coursera.org/maestro/auth/api/user/login', formParams)
-        #print "------------2>", cj
-        #r = opener.open('https://www.coursera.org/')
-
-        print cj
-
-        r = opener.open('https://class.coursera.org/algo/auth/auth_redirector?type=login&subtype=normal&email=&visiting=%2Falgo%2Flecture%2Findex&minimal=true')
-
-        r = opener.open(config.course)
-        #print "------------3>", cj
-
-        print r.read()
+print r.read()
 
 
